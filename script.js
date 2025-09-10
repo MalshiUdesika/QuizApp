149bdf61128c52576536f57c4c0bc853244d9901
MalshiUdesika <malshigamage15.com>
2025-09-10 15:52:58 +0530

    Add questions

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizDate = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d"
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Computer Style Sheets",
+        d: "Creative Style System",
+        correct: "b"
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hyper Trainer Markup Language",
+        b: "Hyper Text Marketing Language",
+        c: "Hyper Text Markup Language",
+        d: "Hyper Tool Multi Language",
+        correct: "c"
+    },
+    {
+        question: "In what year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "1997",
+        correct: "b"
+    }
+];
